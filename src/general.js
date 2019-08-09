e73d24264ad520b6e32f817440da5a828a5316b5
straengel <straengel@gmail.com>
2019-08-10 02:06:52 +0300

    general и main

---
+++ src/general.js
@@ -0,0 +1,13 @@
+'use strict';
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import 'nodelist-foreach-polyfill';
+import 'es6-promise';
+import 'whatwg-fetch';
+//import 'fetch-polyfill';
+import 'formdata-polyfill';
+import elemntClosest from 'element-closest';
+elemntClosest(window);
+
+//пример подключение js файлов
+//import countTimer from "./modules/countTimer";
