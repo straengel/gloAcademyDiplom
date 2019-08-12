af8e0d367a178cce57594a3f7634dd4b96bb09df
Sergey <straengel@gmail.com>
2019-08-12 19:19:35 +0300

    Карусель блин

--- src/modules/general/servicesSlider.js
+++ src/modules/general/servicesSlider.js
@@ -1,52 +1,70 @@
 import animateCSS from "./animateCss";
-const servicesSlider = () => {
+const servicesSlider = (minWidth = 223) => {
     const   slide = document.querySelectorAll('.services-slider .slide'),
             slider = document.querySelector('.services-slider'),
-            html = `
+            sliderWrapper = document.querySelector('#services .wrapper'),
+            maxWidth = minWidth*(slide.length-5);
+    let prevArr,
+        nextArr,
+        currentWidth = 0,
+        h2 = document.querySelector('#services .wrapper h2'),
+        html = `
                 <div class="slider-arrow next"><span><i class="fa fa-angle-right"></i></span></div>
                 <div class="slider-arrow prev"><span><i class="fa fa-angle-left"></i></span></div>
             `;
-    let prevArr,
-        nextArr,
-        numFirstSlide = 0,
-        numLastSlide = 5;
-    slider.style.position = `relative`;
-    slider.insertAdjacentHTML('beforeEnd', html);
-
-    slide.forEach((elem, index) => {
-        if(index > 4){
-            elem.style.display = `none`;
+    slide.forEach((elem,index) => {
+        if(index === 0){
+            elem.style.cssText = `
+                -webkit-transition: .2s linear;
+                -moz-transition: .2s linear;
+                -o-transition: .2s linear;
+                transition: .2s linear;
+                min-width: 210px;
+            `;
+        } else {
+            elem.style.cssText = `min-width: 210px;`;
         }
-    });
 
+    });
+    //корректируем обертку
+    sliderWrapper.style.cssText = `max-width: 1150px`;
+    //освобождаем  wrapper
+    document.getElementById('services').insertAdjacentHTML('afterbegin', h2.outerHTML);
+    h2.remove();
+    //добавляем стрелки стили и анимацию
+    slider.style.cssText = `
+        position: relative;
+        overflow-x: hidden;
+    `;
+    slider.insertAdjacentHTML('beforeEnd', html);
     nextArr = slider.querySelector('.slider-arrow.next');
     prevArr = slider.querySelector('.slider-arrow.prev');
 
+    const moveSlide = (changeSlide) => {
+        currentWidth += +changeSlide;
 
-    const prevSlide = () => {
-        if(numFirstSlide >= 1){
-            numLastSlide--;
-            numFirstSlide--;
-            animateCSS(slide[numFirstSlide], 'fadeInLeft', slide[numFirstSlide].style.display = 'block');
-            slide[numLastSlide].style.display = 'none';
+
+        if(currentWidth <= 6){
+            console.log(currentWidth);
+            currentWidth = 4;
+            slide[0].style.marginLeft = `-${currentWidth}px`;
+            currentWidth = 0;
+        } else if(currentWidth >= maxWidth){
+            currentWidth = maxWidth;
+            slide[0].style.marginLeft = `-${currentWidth}px`;
         }
+        slide[0].style.marginLeft = `-${currentWidth}px`;
+
     };
 
-    const nextSlide = () => {
-        if(numLastSlide < slide.length){
-            animateCSS(slide[numLastSlide], 'fadeInRight', slide[numLastSlide].style.display = 'block');
-            slide[numFirstSlide].style.display = 'none';
-            numLastSlide++;
-            numFirstSlide++;
-        }
-    };
     slider.addEventListener('click', (event) => {
         let target = event.target;
-        if(target.closest('.slider-arrow.next')){
-            nextSlide();
+        event.preventDefault();
+        if(target.closest('.next')){
+            moveSlide(minWidth);
         }
-        if(target.closest('.slider-arrow.prev')){
-            prevSlide();
+        if(target.closest('.prev')){
+            moveSlide(-minWidth);
         }
     });
 };
